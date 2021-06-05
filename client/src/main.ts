dc95d2ebb3b9dd549ea913b4189773beaaa1b460
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2021-06-05 16:27:58 -0400

    Remove "Element-Plus" UI framework and Add "PrimeVue"

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 import "./styles/index.scss";
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(ElementPlus)
+  .use(PrimeVue, { ripple: true })
   .mount("#app");
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,21 +1,22 @@
 <template>
-  <el-row>
-    <el-button>Défaut</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
+  <Button label="Primary" />
+  <Button label="Secondary" class="p-button-secondary" />
+  <Button label="Success" class="p-button-success" />
+  <Button label="Info" class="p-button-info" />
+  <Button label="Warning" class="p-button-warning" />
+  <Button label="Help" class="p-button-help" />
+  <Button label="Danger" class="p-button-danger" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Button from "primevue/button/Button.vue";
 
 @Options({
   components: {
-    HelloWorld
+    HelloWorld,
+    Button
   }
 })
 export default class Home extends Vue {}
