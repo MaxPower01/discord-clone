0b64b81029cd31f57921df5bdb47a208515d2e1b
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2021-06-06 10:01:19 -0400

    Update theme

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,6 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
 import "./styles/index.scss";
 
 createApp(App)
