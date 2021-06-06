3e45fdbbe5e46490dbc31da553bded61fd9bb87a
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2021-06-06 15:19:38 -0400

    Add TopBar

---
+++ client/src/components/TopBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-menu
+    class="el-menu-demo"
+    mode="horizontal"
+    :router="true"
+    :default-active="activeTab"
+    @select="handleSelect"
+  >
+    <el-menu-item index="/">
+      Accueil
+    </el-menu-item>
+    <el-menu-item index="/about">
+      À propos
+    </el-menu-item>
+    <el-menu-item index="/signin">
+      Connexion
+    </el-menu-item>
+    <el-menu-item index="/signup">
+      Créer un compte
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "TopBar",
+  data() {
+    return {
+      activeTab: ""
+    };
+  },
+  setup(props) {
+    function handleSelect(index: string, indexPath: string[]) {
+      console.log(index);
+      console.log(indexPath);
+    }
+
+    return {
+      handleSelect
+    };
+  },
+  watch: {
+    $route(newRoute, oldRoute) {
+      this.activeTab = newRoute.path;
+    }
+  }
+});
+</script>
