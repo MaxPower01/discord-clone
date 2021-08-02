30b55137b18c0546fcf5c01e9a02f4920ea9cb3d
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2021-08-02 09:11:57 -0400

    Update theme

--- client/web/src/services/theme-service.ts
+++ client/web/src/services/theme-service.ts
@@ -42,10 +42,10 @@ export default class ThemeService {
       700: style.getPropertyValue("--color-primary-700").trim(),
       800: style.getPropertyValue("--color-primary-800").trim(),
       900: style.getPropertyValue("--color-primary-900").trim(),
-      A100: style.getPropertyValue("--color-on-primary-100").trim(),
-      A200: style.getPropertyValue("--color-on-primary-200").trim(),
-      A400: style.getPropertyValue("--color-on-primary-400").trim(),
-      A700: style.getPropertyValue("--color-on-primary-700").trim(),
+      A100: style.getPropertyValue("--color-primary-A100").trim(),
+      A200: style.getPropertyValue("--color-primary-A200").trim(),
+      A400: style.getPropertyValue("--color-primary-A400").trim(),
+      A700: style.getPropertyValue("--color-primary-A700").trim(),
     };
 
     const secondaryColor = {
@@ -59,10 +59,10 @@ export default class ThemeService {
       700: style.getPropertyValue("--color-secondary-700").trim(),
       800: style.getPropertyValue("--color-secondary-800").trim(),
       900: style.getPropertyValue("--color-secondary-900").trim(),
-      A100: style.getPropertyValue("--color-on-secondary-100").trim(),
-      A200: style.getPropertyValue("--color-on-secondary-200").trim(),
-      A400: style.getPropertyValue("--color-on-secondary-400").trim(),
-      A700: style.getPropertyValue("--color-on-secondary-700").trim(),
+      A100: style.getPropertyValue("--color-secondary-A100").trim(),
+      A200: style.getPropertyValue("--color-secondary-A200").trim(),
+      A400: style.getPropertyValue("--color-secondary-A400").trim(),
+      A700: style.getPropertyValue("--color-secondary-A700").trim(),
     };
 
     const successColor = {
@@ -76,10 +76,10 @@ export default class ThemeService {
       700: style.getPropertyValue("--color-success-700").trim(),
       800: style.getPropertyValue("--color-success-800").trim(),
       900: style.getPropertyValue("--color-success-900").trim(),
-      A100: style.getPropertyValue("--color-on-success-100").trim(),
-      A200: style.getPropertyValue("--color-on-success-200").trim(),
-      A400: style.getPropertyValue("--color-on-success-400").trim(),
-      A700: style.getPropertyValue("--color-on-success-700").trim(),
+      A100: style.getPropertyValue("--color-success-A100").trim(),
+      A200: style.getPropertyValue("--color-success-A200").trim(),
+      A400: style.getPropertyValue("--color-success-A400").trim(),
+      A700: style.getPropertyValue("--color-success-A700").trim(),
     };
 
     const warningColor = {
@@ -93,10 +93,10 @@ export default class ThemeService {
       700: style.getPropertyValue("--color-warning-700").trim(),
       800: style.getPropertyValue("--color-warning-800").trim(),
       900: style.getPropertyValue("--color-warning-900").trim(),
-      A100: style.getPropertyValue("--color-on-warning-100").trim(),
-      A200: style.getPropertyValue("--color-on-warning-200").trim(),
-      A400: style.getPropertyValue("--color-on-warning-400").trim(),
-      A700: style.getPropertyValue("--color-on-warning-700").trim(),
+      A100: style.getPropertyValue("--color-warning-A100").trim(),
+      A200: style.getPropertyValue("--color-warning-A200").trim(),
+      A400: style.getPropertyValue("--color-warning-A400").trim(),
+      A700: style.getPropertyValue("--color-warning-A700").trim(),
     };
 
     const errorColor = {
@@ -110,15 +110,30 @@ export default class ThemeService {
       700: style.getPropertyValue("--color-danger-700").trim(),
       800: style.getPropertyValue("--color-danger-800").trim(),
       900: style.getPropertyValue("--color-danger-900").trim(),
-      A100: style.getPropertyValue("--color-on-danger-100").trim(),
-      A200: style.getPropertyValue("--color-on-danger-200").trim(),
-      A400: style.getPropertyValue("--color-on-danger-400").trim(),
-      A700: style.getPropertyValue("--color-on-danger-700").trim(),
+      A100: style.getPropertyValue("--color-danger-A100").trim(),
+      A200: style.getPropertyValue("--color-danger-A200").trim(),
+      A400: style.getPropertyValue("--color-danger-A400").trim(),
+      A700: style.getPropertyValue("--color-danger-A700").trim(),
     };
 
-    const infoColor = gray;
+    const neutralColor = {
+      50: style.getPropertyValue("--color-neutral-50").trim(),
+      100: style.getPropertyValue("--color-neutral-100").trim(),
+      200: style.getPropertyValue("--color-neutral-200").trim(),
+      300: style.getPropertyValue("--color-neutral-300").trim(),
+      400: style.getPropertyValue("--color-neutral-400").trim(),
+      500: style.getPropertyValue("--color-neutral-500").trim(),
+      600: style.getPropertyValue("--color-neutral-600").trim(),
+      700: style.getPropertyValue("--color-neutral-700").trim(),
+      800: style.getPropertyValue("--color-neutral-800").trim(),
+      900: style.getPropertyValue("--color-neutral-900").trim(),
+      A100: style.getPropertyValue("--color-neutral-A100").trim(),
+      A200: style.getPropertyValue("--color-neutral-A200").trim(),
+      A400: style.getPropertyValue("--color-neutral-A400").trim(),
+      A700: style.getPropertyValue("--color-neutral-A700").trim(),
+    };
 
-    const neutralColor = gray;
+    const infoColor = neutralColor;
 
     // TODO : Verify if body.hasClass("light-theme") to set theme accordingly
 
@@ -142,13 +157,13 @@ export default class ThemeService {
         MuiCssBaseline: {
           "@global": {
             body: {
-              backgroundColor: neutralColor["600"],
+              backgroundColor: neutralColor["800"],
             },
           },
         },
         MuiAppBar: {
           colorPrimary: {
-            backgroundColor: neutralColor["500"],
+            backgroundColor: neutralColor["700"],
           },
         },
       },
